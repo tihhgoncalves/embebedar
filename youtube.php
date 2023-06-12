19682b0b07149f95d0ed470f69474421da9ff236
Tihh Gonçalves <tihhgoncalves@gmail.com>
2023-06-12 19:40:40 -0300

    Correção de quando é aberto dentro de outro iframe

--- youtube.php
+++ youtube.php
@@ -2,7 +2,6 @@
 <html>
 <head>
     <style>
-
       body{
         overflow: hidden;
       }
@@ -28,6 +27,13 @@
             width: 100%;
             height: 100%;
             object-fit: cover;
+            transition: opacity 0.5s linear;
+            visibility: visible;
+        }
+        .videoWrapper img.hide {
+            opacity: 0;
+            visibility: hidden;
+            transition: visibility 0s 0.5s, opacity 0.5s linear;
         }
         /* Estilo para o botão de play */
         .videoWrapper .playButton {
@@ -62,6 +68,7 @@
         // Variáveis globais para o objeto do player do YouTube e o ID do vídeo
         let player;
         const videoId = '<?= $_GET['id']; ?>'; // Substitua pelo ID do seu vídeo
+        let iniciado = false;
 
         // Função para carregar o player do YouTube
         function onYouTubeIframeAPIReady() {
@@ -74,30 +81,76 @@
                     'onStateChange': onPlayerStateChange
                 }
             });
+
+            // Adiciona um evento de mouseout ao iframe do YouTube
+            const iframe = document.querySelector('.videoWrapper iframe');
+            iframe.addEventListener('mouseout', function() {
+                const coverImage = document.getElementById('coverImage');
+                const playImage = document.getElementById('playImage');
+
+                if(!iniciado){
+                    coverImage.classList.remove('hide');
+                    playImage.classList.remove('hide');
+                }
+            });
         }
 
         // Função para lidar com mudanças de estado do player
         function onPlayerStateChange(event) {
             const coverImage = document.getElementById('coverImage');
             const playImage = document.getElementById('playImage');
+
+            iniciado = true;
+            
             if (event.data == YT.PlayerState.PLAYING) {
-                  coverImage.style.display = 'none';
-                  playImage.style.display = 'none';
-            } else {
-                coverImage.style.display = 'block';
-                playImage.style.display = 'block';
+                //coverImage.style.display = 'none';
+                //playImage.style.display = 'none';
+                coverImage.classList.add('hide');
+                playImage.classList.add('hide');
+            } else if (event.data == YT.PlayerState.PAUSED) {
+                
+                coverImage.classList.remove('hide');
+                playImage.classList.remove('hide');
             }
         }
 
-        // Adiciona um evento de clique às imagens
+        // Adiciona um evento de mouseover às imagens
         const coverImage = document.getElementById('coverImage');
         const playImage = document.getElementById('playImage');
+        
         coverImage.addEventListener('click', function() {
             player.playVideo();
         });
         playImage.addEventListener('click', function() {
             player.playVideo();
         });
+
+        /** O primeiro play nunca funciona via API, porque o iframe do Youtube está dentro de outro iframe, por isso esse esquema */
+        coverImage.addEventListener('mouseover', function() {
+
+          if(!iniciado){
+            coverImage.classList.add('hide');
+            playImage.classList.add('hide');
+          }
+          
+        });
+
+        playImage.addEventListener('mouseover', function() {
+
+          if(!iniciado){
+            coverImage.classList.add('hide');
+            playImage.classList.add('hide');
+            console.log('hide-4');
+          }
+
+        });
+
+
+        //verifica se está dentro de um iframe
+        if (window.self == window.top){
+          iniciado = true;
+        }
+
     </script>
 </body>
 </html>
